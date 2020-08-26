569a56e630d3a8feab904defc7020e73b45b46a5
recai.huseyin@oredata.com <Oredata123//>
2020-08-26 20:54:20 +0300

    edit main page

---
+++ assets/js/translate.js
@@ -0,0 +1,92 @@
+function getLangResources(){
+    var tr = new Array();
+    var en = new Array();
+    var ar = new Array();
+    
+    tr['content_about'] = "Türkiye’de hayallerini gerçekleştirmek isteyen, kendine ve eğitimine yatırım yapmak isteyen, Türkiye’de yaşamak ve çalışmak isteyen, yatırım yapmak isteyen, Türkiye’deki saklı cennetleri keşfetmek isteyen kişilere yardımcı olmak amacıyla kurulmuş ve siz değerli danışanlarımızın taleplerini karşılamak adına emek veren bir kuruluşuz. Bizler DRE CONSULTANCY olarak, siz değerli danışanlarımızın her daim yanında olmaktan ve özverili çalışmaktan memnuniyet duymaktayız. Ekibimiz ve çalışma alanlarımızdaki özel danışmanlarız ile sizlerin hizmetinizdeyiz. Bizler DRE CONSULTANCY olarak, siz değerli danışanlarımızın her daim yanında olmaktan ve özverili çalışmaktan memnuniyet duymaktayız. Ekibimiz ve çalışma alanlarımızdaki özel danışmanlarız ile sizlerin hizmetinizdeyiz."; 
+    en['content_about'] = "english";
+    ar['content_about'] = "شركتنا هي مركز للخدمات الاستشارية في إسطنبول وبورصة. تقدم الشركة خدماتها في مجالات التعليم، الاستثمار، العمل والإقامة في تركيا. نهدف لبذل كل جهدنا لتقديم الدعم الكامل في تلبية طلباتكم بالشكل الأفضل. من خلال طاقمنا المؤهل في مجالاتنا المختلفة نسعى الى كسب ثقتكم وتلبية احتياجاتكم في أقصر وقت ممكن";
+    tr['content_vision'] = "Kurucu ekibimiz olarak, uzun süredir devam eden çalışmalarımızı profesyonel alana taşımak ve sizlere erişilebilirlik sağlamak amacıyla DRE CONSULTANCY’i kurduk. Siz değerli danışanlarımızın hayallerini gerçekleştirmeyi ve her aşamada sizlerin yanında olmayı misyon edindik. Sizlerin mutluluğu ile büyümek ve her zaman daha iyisini yapmak düşüncesiyle, DRE CONSULTANCY olarak yüksek performans sergileyen bir ekip olmaya devam edeceğiz."; 
+    en['content_vision'] = "english";
+    ar['content_vision'] = "نهدف في شركتنا إلى مشاركة خبرتنا في مجالاتنا الاستشارية المتعددة. نسعى إلى تحقيق أحلامكم ودعمكم في جميع المراحل خلال تجربتكم في تركيا. من واجبنا خلق علاقات مبنية على الثقة والاحترام بين العملاء وفريق العمل. هدفنا أن نحقق طلباتكم على أكمل وجه بكفائة عالية";
+    tr['title_about'] = "BİZ KİMİZ"; 
+    en['title_about'] = "About";
+    ar['title_about'] = "من نحن";
+    tr['title_vision'] = "MİSYON VE VİZYONUMUZ"; 
+    en['title_vision'] = "Vision";
+    ar['title_vision'] = "رؤيتنا ورسالتنا";
+    tr['title_team'] = "EKİBİMİZ"; 
+    en['title_team'] = "Team";
+    ar['title_team'] = "فريقنا";
+    tr['title_slogan'] = "DRE Danışmanlık"; 
+    en['title_slogan'] = "DRE Consultancy";
+    ar['title_slogan'] = "شركة دي ار اي الاستشارية";
+    tr['title_mainpage'] = "Anasayfa"; 
+    en['title_mainpage'] = "Home";
+    ar['title_mainpage'] = "الرئيسية";
+    tr['title_services'] = "HİZMETLERİMİZ"; 
+    en['title_services'] = "Services";
+    ar['title_services'] = "خدماتنا";
+    tr['title_blog'] = "Blog"; 
+    en['title_blog'] = "Blog";
+    ar['title_blog'] = "المدونة";
+    tr['title_contact'] = "İLETİŞİM"; 
+    en['title_contact'] = "Contact";
+    ar['title_contact'] = "اتصل بنا";
+    tr['title_language'] = "Türkçe"; 
+    en['title_language'] = "English";
+    ar['title_language'] = "العربية";
+    tr['title_education'] = "EĞİTİM"; 
+    en['title_education'] = "EDUCATION";
+    ar['title_education'] = "التعليم";
+    tr['title_residancy'] = "OTURUM"; 
+    en['title_residancy'] = "RESIDANCY";
+    ar['title_residancy'] = "الإقامة";
+    tr['title_travel'] = "SEYAHAT"; 
+    en['title_travel'] = "TRAVEL";
+    ar['title_travel'] = "السياحة";
+    tr['title_mortgage'] = "GAYRİMENKUL DEĞERLEME VE SATIN ALMA"; 
+    en['title_mortgage'] = "MORTGAGE";
+    ar['title_mortgage'] = "الإستشمار العقاري";
+    tr['name_recai'] = "Rajai Hussein"; 
+    en['name_recai'] = "Rajai Hussein";
+    ar['name_recai'] = "رجائي حسين";
+    tr['name_elif'] = "Elif Polat"; 
+    en['name_elif'] = "Elif Polat";
+    ar['name_elif'] = "ألف بولاط";
+    tr['name_didem'] = "Didem Tenekecioğlu"; 
+    en['name_didem'] = "Didem Tenekecioğlu";
+    ar['name_didem'] = "ديدام تنكجي أوغلو";
+    tr['content_recai'] = "Filistin, Tulkarm doğumlu olan ve öğrenim görme amacıyla Türkiye’ye gelen Rajai Hussein, kendi deneyimleri doğrultusunda, siz değerli danışanlarımız ile yaşadığı kişisel tecrübeyi profesyonelliğe dönüştürmektedir. Ege Üniversitesi Bilgisayar Mühendisliği Bölümü’nden mezun olan Rajai Hussein, uzun yıllardır Türkiye’de eğitim almak isteyen yabancı uyruklu vatandaşlara yardımcı olmakta ve her aşamada destek sunmaktadır. Şirketimizin kuruluş sürecinde önemli adımlar atmamıza destek veren Rajai Hussein, şirketimize yabancı uyruklu öğrencilerin eğitim kayıt, yerleşme ve yaşam planlaması süreçlerinde destek vermektedir. Anadili Arapça olup, ileri düzeyde Türkçe ve İngilizce bilmektedir."; 
+    en['content_recai'] = "Filistin, Tulkarm doğumlu olan ve öğrenim görme amacıyla Türkiye’ye gelen Rajai Hussein, kendi deneyimleri doğrultusunda, siz değerli danışanlarımız ile yaşadığı kişisel tecrübeyi profesyonelliğe dönüştürmektedir. Ege Üniversitesi Bilgisayar Mühendisliği Bölümü’nden mezun olan Rajai Hussein, uzun yıllardır Türkiye’de eğitim almak isteyen yabancı uyruklu vatandaşlara yardımcı olmakta ve her aşamada destek sunmaktadır. Şirketimizin kuruluş sürecinde önemli adımlar atmamıza destek veren Rajai Hussein, şirketimize yabancı uyruklu öğrencilerin eğitim kayıt, yerleşme ve yaşam planlaması süreçlerinde destek vermektedir. Anadili Arapça olup, ileri düzeyde Türkçe ve İngilizce bilmektedir.";
+    ar['content_recai'] = "فلسطيني الجنسية، أكمل تعليمه الجامعي في مجال هندسة الحاسوب في جامعة إيجة في إزمير. بالاستفادة من تجربته الشخصية قام بتقديم خدماته للطلاب الأجانب في تركيا لسنوات عديدة. مؤسس الشركة ومتخصص في مجال التعليم والمعيشة للطلاب في تركيا. لغته الأم هي اللغة العربية ويتحدث اللغة التركية والانجليزية بطلاقة";
+    tr['content_elif'] = "Ege Üniversitesi Bilgisayar Mühendisliği Bölümü’nden ve Anadolu Üniversitesi İşletme Fakültesi’nden mezun olan, uzun yıllardır bankacılık ve yatırım danışmanlığı sektöründe emek veren Elif Polat, şirketimize gayrimenkul değerleme ve yatırım planlaması sürecinde destek vermektedir. Danışanlarımızın mali ve finansal süreçlerinde, deneyimleri doğrultusunda etkin destek sağlamaktadır. Anadili Türkçe olup, ileri düzeyde İngilizce bilmektedir."; 
+    en['content_elif'] = "Ege Üniversitesi Bilgisayar Mühendisliği Bölümü’nden ve Anadolu Üniversitesi İşletme Fakültesi’nden mezun olan, uzun yıllardır bankacılık ve yatırım danışmanlığı sektöründe emek veren Elif Polat, şirketimize gayrimenkul değerleme ve yatırım planlaması sürecinde destek vermektedir. Danışanlarımızın mali ve finansal süreçlerinde, deneyimleri doğrultusunda etkin destek sağlamaktadır. Anadili Türkçe olup, ileri düzeyde İngilizce bilmektedir.";
+    ar['content_elif'] = "تحمل شهادة هندسة الحاسوب من جامعة ايجة وشهادة إدارة الأعمال من جامعة أناضول. متخصصة في مجال البنوك، الاستثمار العقاري، التخطيط والاستشارة المالية. لغتها الأم هي اللغة التركية وتتحدث اللغة الانجليزية بطلاقة";
+    tr['content_didem'] = "Yeditepe Üniversitesi Hukuk Fakültesi’nden mezun olan Avukat Didem TENEKECİOĞLU, hukuki danışmanlık hizmetleri yürütmekte ve şirketimize vize işlemleri, gayrimenkul alımı ve satımı, vatandaşlık başvuruları süreçlerinde destek vermektedir. Anadili Türkçe olup, ileri düzeyde İngilizce bilmektedir."; 
+    en['content_didem'] = "Yeditepe Üniversitesi Hukuk Fakültesi’nden mezun olan Avukat Didem TENEKECİOĞLU, hukuki danışmanlık hizmetleri yürütmekte ve şirketimize vize işlemleri, gayrimenkul alımı ve satımı, vatandaşlık başvuruları süreçlerinde destek vermektedir. Anadili Türkçe olup, ileri düzeyde İngilizce bilmektedir.";
+    ar['content_didem'] = "محامية حصلت على شهادة المحاماة من كلية الحقوق في جامعة يديتبا. مؤسسة الشركة ومتخصصة في أمور الجنسية التركية، أمور الفيزا، بيع وشراء العقارات. لغتها الأم هي اللغة التركية وتتحدث اللغة الانجليزية بطلاقة";
+    tr[''] = "";
+    en[''] = "";
+    ar[''] = "";
+    tr[''] = ""; 
+    en[''] = "";
+    ar[''] = "";
+    
+    var resources = new Array();
+    resources['tr'] = tr;
+    resources['en'] = en;
+    resources['ar'] = ar;
+    
+    return resources;
+}
+
+function changeLanguage(lng){
+var resources = getLangResources()[lng];
+$("[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+document.getElementById("lang_tag").innerHTML = resources["title_language"] + '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+}
+
+changeLanguage("ar");
